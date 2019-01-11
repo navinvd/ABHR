5dcea1e9066116421ae057d398f5bba45c519893
hidnarola <hda@narola.email>
2019-01-11 14:33:58 +0530

    Api doc changes

--- doc/api_project.js
+++ doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-10T09:16:44.705Z",
+    "time": "2019-01-11T09:03:18.636Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
