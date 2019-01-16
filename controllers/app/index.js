d5eb59689c226a332ae3661290de5a58b550369b
Dipesh <dm@narola.email>
2019-01-16 18:25:28 +0530

    made company help api

--- controllers/app/index.js
+++ controllers/app/index.js
@@ -9,6 +9,7 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcrypt');
 var moment = require('moment');
 var mailHelper = require('./../../helper/mail');
+var commonHelper = require('./../../helper/common');
 var ObjectId = require('mongoose').Types.ObjectId;
 var SALT_WORK_FACTOR = config.SALT_WORK_FACTOR;
 var router = express.Router();
@@ -435,6 +436,53 @@ router.post('/forget_password', async(req, res, next) => {
         });
     }
 });
+
+
+// help api
+/**
+ * @api {post} /app/help Company help
+ * @apiName Company help
+ * @apiDescription Get company help
+ * @apiGroup AppUser
+ * @apiVersion 0.0.0
+ * 
+ * @apiParam {Number} help_type [0,1,2,3]
+ * 
+ * @apiHeader {String}  Content-Type application/json    
+ * 
+ * @apiSuccess (Success 200) {String} message Success message.
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+
+router.post('/help', async (req, res) => {
+    var schema = {
+        'help_type': {
+            notEmpty: true,
+            errorMessage: "Enter type from one of this (0, 1, 2, 3)"
+        }
+    };
+    
+    req.checkBody(schema);
+    var errors = req.validationErrors();
+    if (!errors) {
+        var helpResp = await commonHelper.getHelp(req.body.help_type);
+        if(helpResp.status === 'success'){
+            res.status(config.OK_STATUS).json(helpResp)
+        }
+        else{
+            res.status(config.BAD_REQUEST).json(helpResp)
+        }
+    } else {
+        res.status(config.BAD_REQUEST).json({
+            status: 'failed',
+            message: "Validation Error",
+            errors
+        });
+    }
+});
+
+
+
 
 
 module.exports = router;
--- helper/common.js
+++ helper/common.js
@@ -3,6 +3,7 @@ const CarBooking = require('./../models/car_booking');
 const CarBrand = require('./../models/car_brand');
 const CarCompany = require('./../models/car_company');
 const CarModel = require('./../models/car_model');
+const Help = require('./../models/company_help');
 const CarNotification = require('./../models/car_notification');
 const CarNotificationSetting = require('./../models/car_notification_settings');
 const Keywords = require('./../models/keyword');
@@ -64,4 +65,39 @@ commonHelper.getDatabyId = async function (model, id) {
     }
 };
 
-module.exports = carHelper;
+commonHelper.getHelp = async function (help_type) {
+    try {
+        // Help
+        var setKey = '';
+        if(help_type === 0){
+            setKey = 'trips_and_fare_away'
+        }
+        else if(help_type === 1){
+            setKey = 'account_and_payment_options'
+        }   
+        else if(help_type === 2){
+            setKey = 'guide_to_abhr'
+        }   
+        else if(help_type === 3){
+            setKey = 'accessibility'
+        }
+        else{
+            setKey = 'trips_and_fare_away'
+        }   
+        
+        let obj = {};
+        obj[`help.${setKey}`] = 1;
+        const helpDetail = await Help.find({},obj);
+
+        if (helpDetail && helpDetail.length > 0) {
+            return { status: 'success', message: "Help has been found", data: helpDetail }
+        } else {
+            return { status: 'failed', message: "Sorry, currently there is no help" };
+        }
+    } catch (err) {
+        return { status: 'failed', message: "Error occured while get help" };
+    }
+};
+
+
+module.exports = commonHelper;
