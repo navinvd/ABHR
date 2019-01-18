85ffc7483ba399d9d079b86c3aab53c3de73a480
hidnarola <hda@narola.email>
2019-01-18 12:32:04 +0530

    added API for terms and condition

--- controllers/admin/terms_and_conditions.js
+++ controllers/admin/terms_and_conditions.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var config = require('./../../config');
-var User = require('./../../models/users');
-var Company = require('./../../models/car_company');
 var path = require('path');
 var async = require("async");
-var ObjectId = require('mongoose').Types.ObjectId;
 var auth = require('./../../middlewares/auth');
 var moment = require('moment');
 var SALT_WORK_FACTOR = config.SALT_WORK_FACTOR;
@@ -13,12 +10,11 @@ var _ = require('underscore');
 const TermsAndConditionHelper = require('./../../helper/terms_and_condition');
 
 /**
- * @api {get} /admin/terms_and_conditions/:id Details of admin terms and conditions
+ * @api {get} /admin/terms_and_conditions/ Details of admin terms and conditions
  * @apiName Terms And Condition Details
  * @apiDescription To display Terms And Condition Details 
  * @apiGroup Admin - Terms And Condition
  * 
- * @apiParam {user_id} user_id id of User
  * 
  * @apiHeader {String}  Content-Type application/json 
  * @apiHeader {String}  x-access-token Users unique access-key   
@@ -26,29 +22,14 @@ const TermsAndConditionHelper = require('./../../helper/terms_and_condition');
  * @apiSuccess (Success 200) {String} message Success message.
  * @apiError (Error 4xx) {String} message Validation or error message.
  */
-router.get('/terms_and_conditions/:id', async (req, res) => {
-    var schema = {
-        'user_id': {
-            notEmpty: true,
-            errorMessage: "Please enter user id"
+router.get('/', async (req, res) => {
+        const termsAndconditionResp = await TermsAndConditionHelper.getDataByuserId();
+        if (termsAndconditionResp.status === 'success') {
+            res.status(config.OK_STATUS).json(termsAndconditionResp);
         }
-    };
-    req.checkBody(schema);
-    var errors = req.validationErrors();
-    if (!errors) {
-        const termsAndconditionResp = await TermsAndConditionHelper.getDataByuserId(new ObjectId(req.body.user_id));
-        res.json(termsAndconditionResp);
-        // if (carResp.status === 'success') {
-        //     res.status(config.OK_STATUS).json(carResp);
-        // }
-        // else {
-        //     res.status(config.BAD_REQUEST).json(carResp);
-        // }
-        // res.json(carResp);
-    } else {
-        res.status(config.BAD_REQUEST).json({
-            status: 'failed',
-            message: "Validation Error",
-        });
-    }
+        else {
+            res.status(config.BAD_REQUEST).json(termsAndconditionResp);
+        }
 });
+
+module.exports = router;
--- helper/terms_and_condition.js
+++ helper/terms_and_condition.js
@@ -11,7 +11,7 @@ const CarHandOver = require('./../models/car_hand_over');
 const CarReceive = require('./../models/car_receive');
 const CarReport = require('./../models/car_report');
 const CompanyTemrsAndCondition = require('./../models/company_terms_and_condition');
-const TemrsAndCondition = require('./../models/terms_and_conditions');
+const TemrsAndCondition = require('./../models/terms_conditions');
 const moment = require('moment');
 const _ = require('underscore');
 var config = require('./../config');
@@ -22,26 +22,16 @@ var async = require("async");
 let termsandconditionHelper = {};
 
 
-termsandconditionHelper.getDataByuserId = async function (userId) {
+termsandconditionHelper.getDataByuserId = async function () {
     try {
-        console.log("Default Query => ", JSON.stringify(defaultQuery));
-        let admin = await TemrsAndCondition.findOne({"_id": ObjectId(userId), "type": "admin"});
-        return admin;
-        // if (admin && admin.length > 0) {
-
-        //     cars = cars.map((c) => {
-        //         c.car["total_avg_rating"] = c.total_avg_rating;
-        //         delete c.car.reviews;
-        //         return c.car;
-        //     })
-
-        //     return { status: 'success', message: "Car data found", data: { cars: cars } }
-        // } else {
-        //     return { status: 'failed', message: "No car data found" }
-        // }
+        let admin = await TemrsAndCondition.findOne({});
+        if (typeof admin !== 'undefined' && admin !== null) {
+            return { status: 'success', message: "Legal data found", data: admin }
+        } else {
+            return { status: 'failed', message: "No Legal data found" }
+        }
     } catch (err) {
-        console.log("Err : ", err);
-        return { status: 'failed', message: "Error occured while finding car", err };
+        return { status: 'failed', message: "Error occured while finding data", err };
     }
 };
 module.exports = termsandconditionHelper;
