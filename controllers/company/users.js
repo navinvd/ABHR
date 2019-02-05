2f90ac122599eab479ebf4c2da851e40b4878f49
hidnarola <hda@narola.email>
2019-02-05 18:45:33 +0530

    export API Done

--- controllers/company/users.js
+++ controllers/company/users.js
@@ -235,4 +235,196 @@ router.post('/report_list', async (req, res, next) => {
     }
 });
 
+
+/**
+ * @api {post} /company/users/export_report_list create report list for cars
+ * @apiName Listing of users export report list
+ * @apiDescription This is for listing user export report
+ * @apiGroup Admin - Users
+ * @apiVersion 0.0.0
+ * 
+ * @apiParam {String} start pagination start page no
+ * @apiParam {String} end pagination length no of page length
+ * @apiParam {String} company_id companyId 
+ * 
+ * @apiHeader {String}  Content-Type application/json   
+ * @apiHeader {String}  x-access-token Admin unique access-key  
+ * 
+ * @apiSuccess (Success 200) {String} message Success message.
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+router.post('/export_report_list', async (req, res, next) => {
+        var defaultQuery = [
+            {
+                $match: {
+                    "isDeleted": false
+                },
+            },
+            {
+                $lookup: {
+                    from: 'cars',
+                    localField: 'carId',
+                    foreignField: '_id',
+                    as: 'car_details'
+                }
+            },
+            {
+                $unwind: {
+                    "path": "$car_details"
+                }
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'userId',
+                    foreignField: '_id',
+                    as: 'user_details'
+                }
+            },
+            {
+                $unwind: {
+                    "path": "$user_details"
+                }
+            },
+            {
+                $lookup: {
+                    from: 'car_company',
+                    localField: 'car_details.car_rental_company_id',
+                    foreignField: '_id',
+                    as: 'car_compnay'
+                }
+            },
+            {
+                $unwind: '$car_compnay'
+            },
+            {
+                $lookup: {
+                    from: 'car_model',
+                    localField: 'car_details.car_model_id',
+                    foreignField: '_id',
+                    as: 'car_model'
+                }
+            },
+            {
+                $unwind: '$car_model'
+            },
+            {
+                $lookup: {
+                    from: 'car_brand',
+                    localField: 'car_details.car_brand_id',
+                    foreignField: '_id',
+                    as: 'car_brand'
+                }
+            },
+            {
+                $unwind: '$car_brand'
+            },
+            {
+                $match: {
+                    "car_details.car_rental_company_id" : new ObjectId(req.body.company_id)
+                }
+            }];
+            defaultQuery.push(
+                {
+                    $project: {
+                        _id: 1,
+                        no_of_rented: 1,
+                        company_name: "$car_compnay.name",
+                        from_time: 1,
+                        to_time: 1,
+                        booking_rent: 1,
+                        isDeleted: 1,
+                        trip_status:1,
+                        first_name: "$user_details.first_name",
+                        last_name:"$user_details.last_name"
+                    }
+                });
+            if (req.body.selectFromDate && req.body.selectToDate) {
+                var From_date = moment(req.body.selectFromDate).utc();
+                var To_date = moment(req.body.selectToDate).utc();
+                defaultQuery.push({
+                    $match: {
+                        $and:[
+                            { "from_time": { $gte: new Date(req.body.selectFromDate) } },
+                            { "to_time": { $lte: new Date(req.body.selectToDate) } },
+                        ]
+                    },
+                })
+            }
+        if (typeof req.body.search !== 'undefined' && req.body.search !== null && Object.keys(req.body.search).length > 0 && req.body.search.value !== '') {
+            if (req.body.search.value != undefined) {
+                var regex = new RegExp(req.body.search.value);
+                var match = { $or: [] };
+                req.body['columns'].forEach(function (obj) {
+                    if (obj.name) {
+                        var json = {};
+                        if (obj.isNumber) {
+                            json[obj.name] = parseInt(req.body.search.value)
+                        } else {
+                            json[obj.name] = {
+                                "$regex": regex,
+                                "$options": "i"
+                            }
+                        }
+                        match['$or'].push(json)
+                    }
+                });
+            }
+            console.log('re.body.search==>', req.body.search.value);
+            var searchQuery = {
+                $match: match
+            }
+            defaultQuery.push(searchQuery);
+            console.log("==>", JSON.stringify(defaultQuery));
+        }
+        if (typeof req.body.order !== 'undefined' && req.body.order.length > 0) {
+            var colIndex = req.body.order[0].column;
+            var colname = req.body.columns[colIndex].name;
+            colname = '$' + colname;
+            var order = req.body.order[0].dir;
+            if (order == "asc") {
+                defaultQuery = defaultQuery.concat({
+                    $project: {
+                        "records": "$$ROOT",
+                        "sort_index": { "$toLower": [colname] }
+                    }
+                },
+                    {
+                        $sort: { "sort_index": 1 }
+                    },
+                    {
+                        $replaceRoot: { newRoot: "$records" }
+                    })
+            } else {
+                defaultQuery = defaultQuery.concat({
+                    $project: {
+                        "records": "$$ROOT",
+                        "sort_index": { "$toLower": [colname] }
+                    }
+                },
+                    {
+                        $sort: {
+                            "sort_index": -1
+                        }
+                    },
+                    {
+                        $replaceRoot: { newRoot: "$records" }
+                    })
+            }
+        }
+        var totalrecords = await CarBooking.aggregate(defaultQuery);
+        console.log('defaultQuery===>', JSON.stringify(defaultQuery));
+        CarBooking.aggregate(defaultQuery, function (err, data) {
+            console.log('data===>', data);
+            if (err) {
+                return next(err);
+            } else {
+                res.status(config.OK_STATUS).json({
+                    message: "Success",
+                    result: { recordsTotal: totalrecords.length, data: data }
+                });
+            }
+        })
+});
+
 module.exports = router;
