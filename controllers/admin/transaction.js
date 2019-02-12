64156b88d4da044d3ec981523c52d4776790fbd0
hidnarola <hda@narola.email>
2019-02-12 12:56:16 +0530

    transaction report changes done

--- controllers/admin/transaction.js
+++ controllers/admin/transaction.js
@@ -428,16 +428,16 @@ router.post('/export_report_list', async (req, res, next) => {
               }
             }
           ];
-        if (req.body.selectFromDate && req.body.selectToDate) {
-            var From_date = moment(req.body.date).utc();
-            var To_date = moment(req.body.date).utc();
+          if (req.body.selectFromDate && req.body.selectToDate) {
+            var From_date = moment(req.body.selectFromDate).utc().startOf('day');
+            var To_date = moment(req.body.selectToDate).utc().startOf('day');
             defaultQuery.push({
                 $match: {
-                    $and:[
-                        { "from_time": { $lte: To_date } },
-                        { "to_time": { $gte: From_date } },
-                    ]
-                },
+                      $and: [
+                                { "from_time": { $gte: new Date(From_date) } },
+                                { "to_time": { $lte: new Date(To_date) } },
+                            ]
+                        }
             })
         }
         console.log('defaultQuery', JSON.stringify(defaultQuery));
